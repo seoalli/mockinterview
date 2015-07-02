f19c53db7f8c463fe81aa9281533b4e201627c5c
majimenez <marysabel.jimenez@careerbuilder.com>
2015-07-02 15:00:14 -0400

    changes to email.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,10 +3,10 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-<<<<<<< HEAD
+
     @url  = 'http://mockinterview.com'
-=======
->>>>>>> origin/master
+
+
     mail(to: @user.email, subject: 'Welcome to Mock Interview Day')
   end
 
