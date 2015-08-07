5a0755ab09fc5fc28cf24ce4f5799debd290fe95
majimenez <marysabel.jimenez@careerbuilder.com>
2015-08-06 21:41:19 -0400

    changes to email message

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,21 +5,11 @@ default :from => 'cbmid@careerbuilder.com'
   def welcome_email(user)
     @user = user
 
-    #@user.debugger
     @url  = 'http://mockinterview.com'
-    #binding.pry
+
     mail( :to => user.email,
           :subject => 'Welcome to Mock Interview Day')
 
-=begin
-    Mail.deliver do
-      to user.email
-      from 'cbmid@careerbuilder.com'
-      subject 'Welcome to Mock Interview Day'
-      body "welcome to mock interview day!"
-
-    end
-=end
   end
 
 end
