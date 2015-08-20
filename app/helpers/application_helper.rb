f48331dc18068583d8f13068232ddfbc4ee60c6c
majimenez <marysabel.jimenez@careerbuilder.com>
2015-08-20 13:37:27 -0400

    adding meta_tags to header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def page_title(separator = " – ")
+    ['CareerBuilder Mock Interview Day', content_for(:title) ].compact.join(separator)
+  end
+
+  def page_heading(title)
+    content_for(:title){ title }
+    content_tag(:h1, title)
+  end
+
+  def page_description
+    ['',content_for(:meta)]
+  end
+  def page_meta_description(description)
+    content_for(:description){ description }
+    content_tag(:head, description)
+  end
+
 end
